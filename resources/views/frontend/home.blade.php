8ddc6ab3a8f8189891cedc2999421a4bf16295d8
bhabish1122 <bhabish1122@gmail.com>
2023-07-27 17:10:32 +0545

    round

--- resources/views/frontend/home.blade.php
+++ resources/views/frontend/home.blade.php
@@ -223,7 +223,7 @@
                 <div class="modal-content">
                   <div class="modal-header">
                     <h1 class="modal-title fs-5" id="exampleModalLabel">Subscribe to our news letter?</h1>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close">&times;</button>
+                    <button type="button"  class="btn btn-primary rounded-circle" data-bs-dismiss="modal" aria-label="Close">&times;</button>
                   </div>
                   <div class="modal-body">
                         <form method="post" id="formNewsLetter" action="{{ route('store.newsletter') }}">
